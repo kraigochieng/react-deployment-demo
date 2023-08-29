01d70789ae719b81621628b29d6e48c80da337be
kraigochieng <kraigochieng@gmail.com>
2023-08-29 12:30:50 +0300

    Updated project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() { 
+function App() {
   return (
     <>
-     <p>This is my website</p> 
-    </> 
+      <p>This is my website</p>
+    </>
   )
 }
 
